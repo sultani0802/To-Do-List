7b442b942bd33aab69299f075270f7fc5be973c6
sultani0802 <haamedsultani@cmail.carleton.ca>
2018-09-22 12:45:42 -0400

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Haamed Sultani on 2018-09-22.
+//  Copyright © 2018 Sultani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
