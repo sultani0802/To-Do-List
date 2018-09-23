c2254b06097080b97a39b8654cada548caecb793
sultani0802 <haamedsultani@cmail.carleton.ca>
2018-09-23 18:36:30 -0400

    Completed app with all basic functionality using Core Data

--- To Do List/Controllers/CategoryTableViewController.swift
+++ To Do List/Controllers/CategoryTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 
 class CategoryTableViewController: UITableViewController {
-
+    
     // MARK: - Constants
     let SEGUE_IDENTIFIER: String = "ShowListSegue"
     let CELL_IDENTIFIER: String = "CategoryCell"
@@ -18,10 +18,12 @@ class CategoryTableViewController: UITableViewController {
     
     // MARK: - Variables
     var categoryArray : [Category] = [Category]()
+    weak var addActionToEnable: UIAlertAction?
     
     // MARK: - View Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         loadData()
     }
     
@@ -31,13 +33,15 @@ class CategoryTableViewController: UITableViewController {
         do {
             try context.save() // Save the data into our DB
         } catch {
-            print("Error when trying to save to database: \(error)")
+            print("Error when trying to save category to database: \(error)")
         }
         
         tableView.reloadData() // Update the UI
     }
     
-    func loadData(with request: NSFetchRequest<Category> = Category.fetchRequest()){
+    func loadData(){
+        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        
         do {
             categoryArray = try context.fetch(request) // Load the data from the local database
         } catch {
@@ -46,44 +50,86 @@ class CategoryTableViewController: UITableViewController {
         
         tableView.reloadData() // Update the UI with the data
     }
-
+    
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categoryArray.count
     }
-
-    /*
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_IDENTIFIER, for: indexPath)
+        
+        let category = categoryArray[indexPath.row]
+        cell.textLabel?.text = category.name
+        
         return cell
     }
-    */
-
-
-
-    /*
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: SEGUE_IDENTIFIER, sender: self)
+    }
+    
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let destVC = segue.destination as! TodoListTableViewController
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destVC.selectedCategory = categoryArray[indexPath.row]
+            print(categoryArray[indexPath.row].name)
+        }
     }
-    */
-
+    
+    
     
     // MARK: - IB Action Methods
     
     @IBAction func addCategoryButtonPressed(_ sender: Any) {
+        var textField: UITextField = UITextField()
+        let alertController = UIAlertController(title: "Add a new category", message: "", preferredStyle: .alert)
+        
+        // This closure is executed now
+        alertController.addTextField {
+            (alertTextField) in
+            alertTextField.placeholder = "Enter category name..."
+            alertTextField.addTarget(self, action: #selector(self.addCategoryTextChanged(_:)), for: .editingChanged) // Enable Add button when user starts typing
+            textField = alertTextField
+        }
+        
+        // User clicks 'Add' button
+        let addAction = UIAlertAction(title: "Add", style: .default) {
+            (action) in
+            if textField.text?.trimmingCharacters(in: .whitespaces).isEmpty == false { // Check if the text is empty
+                
+                // Instantiate a new instance of Category
+                let newCategory = Category(context: self.context)
+                newCategory.name = textField.text?.trimmingCharacters(in: .whitespaces)
+                
+                self.categoryArray.append(newCategory) // Add to our tableView
+                self.saveData() // Update the UI
+            }
+        }
+        
+        // User cancel's addition of new category
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        self.addActionToEnable = addAction
+        addAction.isEnabled = false
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @objc func addCategoryTextChanged(_ sender: UITextField) {
+        self.addActionToEnable?.isEnabled = true
     }
 }
--- To Do List/Controllers/TodoListTableViewController.swift
+++ To Do List/Controllers/TodoListTableViewController.swift
@@ -19,7 +19,12 @@ class TodoListTableViewController: UITableViewController {
     // MARK: - Variables
     var itemArray : [Item] = [Item]()
     weak var addActionToEnable: UIAlertAction?
-    
+    // Call loadData() once this variable has a value
+    var selectedCategory: Category? {
+        didSet {
+            loadData()
+        }
+    }
     
     // MARK: - IB Outlets
     @IBOutlet weak var searchBar: UISearchBar!
@@ -28,10 +33,6 @@ class TodoListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
-        loadData()
-        
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(TodoListTableViewController.dismissKeyboard))
-        view.addGestureRecognizer(tap)
     }
     
     // This method is called when we detect that the user started typing in a new list item
@@ -51,9 +52,16 @@ class TodoListTableViewController: UITableViewController {
     }
     
     
-    func loadData(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
-        // Go into database and get a reference to the Entity we want
-        // If we don't pass in an NSFetchRequest, then, load all the data in the database
+    func loadData(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        // Go into database and get a reference to the Category we want we want with any additional predicates
+        // If we don't pass in an NSFetchRequest,
+        // If we don't pass in a predicate, then fetch all the items in the category
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        if let pred = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, pred])
+        } else {
+            request.predicate = categoryPredicate
+        }
         
         do {
             itemArray = try context.fetch(request) // Try and get that data from the context
@@ -91,6 +99,10 @@ class TodoListTableViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true) // Deselect the cell
     }
     
+    override func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        self.searchBar.resignFirstResponder()
+    }
+    
     
     // MARK: - IB Actions
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -111,7 +123,9 @@ class TodoListTableViewController: UITableViewController {
                 let newItem = Item(context: self.context) // Create an instance of our Item entity in our Data Model
                 newItem.title = textField.text! // Must instantiate the properties because we declared that it can't be optional in our Data Base
                 newItem.checked = false
+                newItem.parentCategory = self.selectedCategory
                 self.itemArray.append(newItem) // Add the new Item to our array
+                
                 self.saveData()
             }
         }
@@ -135,12 +149,12 @@ extension TodoListTableViewController: UISearchBarDelegate {
         if searchBar.text?.trimmingCharacters(in: .whitespaces).isEmpty == false {
             let request : NSFetchRequest<Item> = Item.fetchRequest()
             let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!.trimmingCharacters(in: .whitespaces)) // Search for substrings that aren't case and diacratic sensitive and remove any extra spaces
-            let sortDescriptor = NSSortDescriptor(key: "title", ascending: true) // Sort using 'title' in alphabetical order
+            let sortDescriptor = NSSortDescriptor(key: "title", ascending: true) // Sort in alphabetical order based on the item's title
             
             request.predicate = predicate
             request.sortDescriptors = [sortDescriptor]
             
-            loadData(with: request)
+            loadData(with: request, predicate: predicate)
         }
     }
     
@@ -150,10 +164,5 @@ extension TodoListTableViewController: UISearchBarDelegate {
         if searchBar.text?.count == 0 {
             loadData()
         }
-    }
-    
-    // Hide the keyboard if the user taps outside the keyboard
-    @objc func dismissKeyboard(){
-        self.searchBar.endEditing(true)
     }
 }
